d0c783af62b364a6aabde3537a33ef5a1b157732
nawhi <25394965+nawhi@users.noreply.github.com>
2022-05-03 18:10:30 +0100

    Don't allow Tailwind default colours anymore

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,17 +7,17 @@ const config = {
     extend: {
       screens: {
         xs: '480px'
-      },
-      colors: {
-        'th-background': 'var(--th-background)',
-        'th-primary': 'var(--th-primary)',
-        'th-secondary': 'var(--th-secondary)',
-        'th-tertiary': 'var(--th-tertiary)',
-        'th-subtle': 'var(--th-subtle)',
-        'th-action': 'var(--th-action)',
-        'th-action-focus': 'var(--th-action-focus)',
-        'th-error': 'var(--th-error)'
       }
+    },
+    colors: {
+      'th-background': 'var(--th-background)',
+      'th-primary': 'var(--th-primary)',
+      'th-secondary': 'var(--th-secondary)',
+      'th-tertiary': 'var(--th-tertiary)',
+      'th-subtle': 'var(--th-subtle)',
+      'th-action': 'var(--th-action)',
+      'th-action-focus': 'var(--th-action-focus)',
+      'th-error': 'var(--th-error)'
     }
   },
   darkMode: 'media',
