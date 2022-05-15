d97fe4a7789659de7b255c262b9e059a3146c17d
nawhi <25394965+nawhi@users.noreply.github.com>
2022-05-15 09:12:45 +0100

    Improve blog layout and header sizing on mobile

--- src/lib/blog/layout.svelte
+++ src/lib/blog/layout.svelte
@@ -1,6 +1,6 @@
 <script>
-  import Header from '$lib/components/Header.svelte';
   import PageTitle from '$lib/components/PageTitle.svelte';
+  import Sitemap from '$lib/components/Sitemap.svelte';
 
   const { title, date, description } = $$restProps;
 
@@ -12,10 +12,11 @@
   })();
 </script>
 
+<Sitemap />
 <PageTitle {title} />
-<Header image={false} />
-<h1 class="mt-12 text-4xl text-th-primary font-bold">{title}</h1>
-<p class="text-th-secondary font-light pt-1.5">
+<h1 class="mt-6 text-3xl xs:text-4xl text-th-primary font-bold">{title}</h1>
+<p class="text-th-secondary pt-2 pb-1 font-serif text-lg">{description}</p>
+<p class="text-th-tertiary font-light pt-1">
   Posted on {formattedDate}
 </p>
 
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import SocialIcons from '$lib/components/SocialIcons.svelte';
   import Sitemap from '$lib/components/Sitemap.svelte';
   import { siteMetadata } from '$lib/content/meta';
@@ -15,7 +15,7 @@
     </a>
   </div>
   <div class="flex-none pt-6 md:pt-1 md:flex-1 md:pl-20">
-    <h1 class="text-5xl text-th-primary font-bold leading-tight hover:text-block">
+    <h1 class="text-4xl xs:text-5xl text-th-primary font-bold leading-tight hover:text-block">
       <a href="/">{siteMetadata.name}</a>
     </h1>
     <p class="text-th-secondary">{siteMetadata.description}</p>
