dc547344d42ed1866258d80bcf17ea8fce46307c
nawhi <25394965+nawhi@users.noreply.github.com>
2022-05-15 09:28:46 +0100

    Use wider letter spacing for blog description

--- src/lib/blog/layout.svelte
+++ src/lib/blog/layout.svelte
@@ -15,7 +15,7 @@
 <Sitemap />
 <PageTitle {title} />
 <h1 class="mt-6 text-3xl xs:text-4xl text-th-primary font-bold">{title}</h1>
-<p class="text-th-secondary pt-2 pb-1 font-serif text-lg">{description}</p>
+<p class="text-th-secondary pt-2 pb-1 font-serif tracking-wide text-lg">{description}</p>
 <p class="text-th-tertiary font-light pt-1">
   Posted on {formattedDate}
 </p>
